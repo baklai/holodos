d102aead708cd05941449b0a7f75d2c5d1c2af96
Dmitrii <d.n.baklai@gmail.com>
2022-07-10 11:28:51 +0300

    edit content

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,8 @@
-const productItems = document.getElementsByClassName('product-item');
+const tg = window.Telegram.WebApp;
 
 const products = [];
+
+const productItems = document.getElementsByClassName('product-item');
 
 Array.from(productItems).forEach((element) => {
   element.addEventListener('click', () => {
@@ -14,18 +16,6 @@ Array.from(productItems).forEach((element) => {
   });
 });
 
-let tg = window.Telegram.WebApp;
-
-console.log(tg);
-
-tg.expand();
-
-tg.MainButton.text = 'Changed Text'; //изменяем текст кнопки
-tg.MainButton.setText('Changed Text1'); //изменяем текст кнопки иначе
-tg.MainButton.textColor = '#F55353'; //изменяем цвет текста кнопки
-tg.MainButton.color = '#143F6B'; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({ color: '#143F6B' }); //так изменяются все параметры
-
 // tg.onEvent('mainButtonClicked', function () {
 //   alert(products);
 //   tg.sendData(JSON.stringify(products));
@@ -37,5 +27,5 @@ const btn = document.getElementById('mainButtonClicked');
 btn.addEventListener('click', function () {
   // alert(products);
   //вешаем событие на нажатие html-кнопки
-  tg.sendData(products);
+  tg.sendData(JSON.stringify(products));
 });
