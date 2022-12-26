9ce892ebddadbbb789ae4778864cade081de9796
Dmitrii <d.n.baklai@gmail.com>
2022-12-26 21:34:03 +0200

    fix loading component

--- components/loading.vue
+++ components/loading.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="sk-grid" v-if="loading">
+  <div class="sk-grid">
     <div class="sk-grid-cube"></div>
     <div class="sk-grid-cube"></div>
     <div class="sk-grid-cube"></div>
@@ -15,17 +15,7 @@
 <script>
 export default {
   data() {
-    return {
-      loading: true
-    };
-  },
-  methods: {
-    start() {
-      this.loading = true;
-    },
-    finish() {
-      this.loading = false;
-    }
+    return {};
   }
 };
 </script>
